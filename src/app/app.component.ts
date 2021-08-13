855fe620b613717828afbc034ba22d9a6ce1ad2b
Lavinya Correia <lavinya.correia01@gmail.com>
2021-08-13 17:02:44 -0300

    Botão de sair caso user esteja autenticado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpService } from './servicos/http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'autenticacao';
+  constructor(private router: Router,
+              private httpService: HttpService) { }
+
+  ngOnInit(): void {
+  }
+
+  sair() {
+    delete localStorage['token'];
+    this.router.navigate(['/login']);
+  }
+
+  autenticado(){
+    return this.httpService.autenticado();
+  }
+
 }
--- src/app/componentes/lancamentos/lancamentos.component.ts
+++ src/app/componentes/lancamentos/lancamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lancamentos',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  sair() {
+    delete localStorage['token'];
+    this.router.navigate(['/login']);
   }
 
 }
