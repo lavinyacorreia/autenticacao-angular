e380d3c08ea09ffd8e7f6f872be91b5113b6d261
Lavinya Correia <lavinya.correia01@gmail.com>
2021-08-13 16:30:15 -0300

    Implementação da autenticação - se autenticado com token entra na tela de lançamentos, senão sempre é direcionado para a tela de Login

--- src/app/servicos/http.service.ts
+++ src/app/servicos/http.service.ts
@@ -6,4 +6,20 @@ import { Injectable } from '@angular/core';
 export class HttpService {
 
   constructor() { }
+
+  autenticado(): boolean {
+  	if (!localStorage['token']) {
+  	  return false;
+  	}
+    try {
+      const dadosUsuario = JSON.parse(atob(localStorage['token'].split('.')[1]));
+      if (!dadosUsuario) {
+        return false;
+      }
+    	return parseInt(dadosUsuario.id) > 0;
+    } catch (error) {
+      return false;
+    }
+  }
+
 }
