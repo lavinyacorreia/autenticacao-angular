a8b2fb927e4e02cbb38e8c0bf81313c2ae52758e
Lavinya Correia <lavinya.correia01@gmail.com>
2021-08-17 18:02:05 -0300

    download dos lançamentos de ponto em excel

--- src/app/servicos/lancamento.service.ts
+++ src/app/servicos/lancamento.service.ts
@@ -2,7 +2,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment as env } from 'src/environments/environment';
+import { Lancamento } from '../models/lancamento.model';
 import { HttpService } from './http.service';
+import { saveAs } from 'file-saver';
 
 @Injectable({
   providedIn: 'root'
@@ -22,4 +24,18 @@ export class LancamentoService {
       this.httpService.headers()
     );
   }
+
+  downloadCSV(lancamentos: Lancamento[]) {
+    const colunas = 'ID,Data,Hora,Tipo,Localização\n';
+    const linhas: string[] = [];
+    lancamentos.forEach(lanc => {
+      const dataHora = lanc.data.split(' ');
+      const linha = `${lanc.id},${dataHora[0]},${dataHora[1]},${lanc.tipo},"${lanc.localizacao}"`;
+      linhas.push(linha);
+    });
+    const dados = colunas + linhas.join('\n');
+    const blob = new Blob([dados], { type: 'text/csv' });
+    saveAs(blob, 'lancamentos.csv');
+  }
+
 }
